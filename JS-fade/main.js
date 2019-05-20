37c336d3d1b970a0ec317e68ea9ffaf5963abe62
Manojlo Ilić <50624481+milic12@users.noreply.github.com>
2019-05-20 12:17:58 +0200

    Update main.js

--- JS-fade/main.js
+++ JS-fade/main.js
@@ -1,3 +1,15 @@
 $('img').load(function() {
     $(this).fadeIn('slow');
   });
+
+  $('.transition-link').on("click", function () {
+
+    // get the href attribute
+    var newUrl = $(this).attr("href");
+
+    // veryfy if the new url exists or is a hash
+    if (!newUrl || newUrl[0] === "#") {
+        // set that hash
+        location.hash = newUrl;
+        return;
+    }
